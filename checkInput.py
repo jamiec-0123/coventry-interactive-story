e846b4fcc85a439cad521c3c7fb30224cdeed67d
Jamie Curran <jamie@jamie-curran.com>
2020-09-29 07:23:26 +0100

    correct way of using try except, my bad.

--- checkInput.py
+++ checkInput.py
@@ -1,12 +1,14 @@
 def checkInput(input, choices):
+    option=0
+    limmit = 0
     try:
         option =int(input)
         limmit = int(choices)
-        if(option>0 and option<limmit):
-            return option
-        else:
-            return 0
     except:
+        return 0
+    if(option>0 and option<limmit):
+        return option
+    else:
         return 0
 
 
