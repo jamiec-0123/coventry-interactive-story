03ccae25e239c09b0831c345fc3061111887847a
Jamie Curran <jamie@jamie-curran.com>
2020-09-30 16:10:51 +0100

    Check to ensure that input isnt a decimal.

--- checkInput.py
+++ checkInput.py
@@ -2,8 +2,11 @@ def checkInput(input, choices):
     option=0
     limmit = 0
     try:
+        """Ensures that the input is an interger"""
         option =int(input)
         limmit = int(choices)
+        if(type(option) != type(input) or type(limmit) != type(choices)):
+            return 0
     except:
         return 0
     if(option>0 and option<limmit):
