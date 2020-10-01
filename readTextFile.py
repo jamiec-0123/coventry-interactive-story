df552b54de01917144ad0b5c089f5739d7c28e57
ben sabatier <bensab62@gmail.com>
2020-10-01 13:04:57 +0100

    read text files

---
+++ readTextFile.py
@@ -0,0 +1,8 @@
+filepath = 'dataDump.txt'
+with open(filepath, 'r', encoding='utf8') as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       line = fp.readline()
+       cnt += 1
