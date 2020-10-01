a5c4f3cf0c51970eb5a13f0e34eb78f08dee0f08
Ben Sabatier <BenS13@users.noreply.github.com>
2020-10-01 13:06:31 +0100

    Update readTextFile.py

--- readTextFile.py
+++ readTextFile.py
@@ -6,3 +6,4 @@ with open(filepath, 'r', encoding='utf8') as fp:
        print("Line {}: {}".format(cnt, line.strip()))
        line = fp.readline()
        cnt += 1
+#output eg. Line 2: The Fly on Doritos
