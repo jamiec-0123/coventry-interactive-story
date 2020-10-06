c1cb937106bcf632f3160302a4ed283f33147300
ben sabatier <bensab62@gmail.com>
2020-10-06 19:19:46 +0100

    'changes'

--- cliMain.py
+++ cliMain.py
@@ -1,1 +1,8 @@
 #Main program
+import checkInput, readTextFile
+
+#print("Fly of The Dorito")
+p=1
+paragraph1 = readTextFile.lineReader(2)
+paragraphStr1 = readTextFile.niceOutput(paragraph1)
+print(paragraphStr1)
