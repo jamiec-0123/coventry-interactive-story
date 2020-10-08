f7c3d347b2f877233a07b65a0918546bc5d096f6
ben sabatier <bensab62@gmail.com>
2020-10-08 17:00:47 +0100

    fixed some syntax errors in cliMain.py

--- cliMain.py
+++ cliMain.py
@@ -7,17 +7,18 @@ paragraph1 = readTextFile.lineReader(0)
 print(paragraph1[0])
 print(paragraph1[1])
 print(paragraph1[2])
-"""
 
+
+'''
 psudo code
-lineuNum=0
+lineNum=0
 userChoice=""
-while(True):
+while True:
     numberOfChoices=0
     if(lineNum !=187):
         data=readTextFile.getLine(lineNum)
         lengthOfText=data[data.length()-1]-lineNum
-        while x is in range(lengthOfText):
+        while x in range(lengthOfText):
             if(data[x].contains("(")):
                 print(data[x])
             elif(type(data[x]) != int ):
@@ -29,7 +30,7 @@ while(True):
         number=0
         while(selected == False):
             if(validateChoice(number,options) !=0):
-                data=findChoice(number-1):
+                data=findChoice(number-1)
             else:
                 number= input(choice)
         
@@ -39,4 +40,4 @@ while(True):
         lineNum=0
     else:
         break
-"""
+'''
